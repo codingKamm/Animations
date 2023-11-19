09d257ed0301b50e0852700b4f0537e706539118
Cameron Warner <cameron.w.warner@gmail.com>
2023-11-19 15:42:22 -0500

    updated transition animation with custom struct;

--- Animations/TransitionsAnimationView.swift
+++ Animations/TransitionsAnimationView.swift
@@ -22,7 +22,13 @@ struct TransitionsAnimationView: View {
                     .fill(.orange)
                     .frame(width: 300, height: 300)
                     //.transition(.scale) //the rectangle scales up as the button makes space, then scales down when tapped again.
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingOrange.toggle()
             }
         }
         
@@ -31,4 +37,24 @@ struct TransitionsAnimationView: View {
 
 #Preview {
     TransitionsAnimationView()
+}
+
+
+struct CornerRotateModifer: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifer(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifer(amount: 0, anchor: .topLeading))
+    }
 }
