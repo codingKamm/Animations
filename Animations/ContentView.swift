bc030c487078ba5bc1a2c381e4f9a79e3e8ebbd8
Cameron Warner <cameron.w.warner@gmail.com>
2023-11-19 12:40:33 -0500

    used rotation 3D effect to animate a button

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("tap Me"){
+                withAnimation(.easeInOut(duration: 2)) {
+                    animationAmount += 360
+                }
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .rotation3DEffect(.degrees(animationAmount),axis: (x: 1.0, y: 1.0, z: 7.0))
+            //  If we skewer the view through the X axis (horizontally) then it will be able to spin forwards and backwards.
+            //  If we skewer the view through the Y axis (vertically) then it will be able to spin left and right.
+            //  If we skewer the view through the Z axis (depth) then it will be able to rotate left and right.
         }
-        .padding()
+
+
     }
 }
 
